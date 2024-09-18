724a6518e139398452e4239a9e1d4d3be0bfc307
Sei <test@test.co.jp>
2024-09-18 11:07:49 +0900

    Adding a Verification Code Using the ucwords Function

--- sample2-1.php
+++ sample2-1.php
@@ -3,4 +3,14 @@
   $off = 0.79;
 
   printf('Price : $%.2f', $price - $off);
+
+  echo "<br>";
+
+  echo ucwords(strtoupper('sato ichiro'));
+  echo "<br>";
+  echo ucwords(strtolower('SATO ICHIRO'));
+  echo "<br>";
+  echo strtoupper(ucwords('sato ichiro'));
+  echo "<br>";
+  echo strtolower(ucwords('SATO ICHIRO'));
 ?>
